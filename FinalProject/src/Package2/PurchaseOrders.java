938ab6343b5b10eff98ce7561ef5e98cae67b495
ZachBrier <zachbrier1@gmail.com>
2018-11-07 15:51:29 -0600

    Set up basic structure of PurchaseOrders

--- FinalProject/src/Package2/PurchaseOrders.java
+++ FinalProject/src/Package2/PurchaseOrders.java
@@ -1,5 +1,42 @@
 package Package2;
 
-public class PurchaseOrders {
+import java.util.Date;
 
+public class PurchaseOrders {
+	private Date date;
+	private String estArrivalTime;
+	private int ordSize;
+	
+	public PurchaseOrders() {
+		
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getEstArrivalTime() {
+		return estArrivalTime;
+	}
+
+	public void setEstArrivalTime(String estArrivalTime) {
+		this.estArrivalTime = estArrivalTime;
+	}
+
+	public int getOrdSize() {
+		return ordSize;
+	}
+
+	public void setOrdSize(int ordSize) {
+		this.ordSize = ordSize;
+	}
+	
+	public String toString() {
+		String str = " ";
+		return str;
+	}
 }
