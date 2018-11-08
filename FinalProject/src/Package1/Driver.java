eab002ff4e36716f84984a95cc6346f851efa0e5
bplichtscheidl <44377292+bplichtscheidl@users.noreply.github.com>
2018-11-08 00:58:13 -0600

    Update Driver.java

--- FinalProject/src/Package1/Driver.java
+++ FinalProject/src/Package1/Driver.java
@@ -1,30 +1,52 @@
 package Package1;
+import Package2.*;
 
 public class Driver {
 
 	public static void main(String[] args) {
 	
 		Inventory inv = new Inventory();
-		Product product1 = new Product("Paper", 10, 59.99, 000000000);
+		BackOrder bo = new BackOrder();
+		InvReOrder re = new InvReOrder();
+		Product product1 = new Product("Paper", 3, 59.99, 000000000);
 		Product product2 = new Product("Pickles", 5, 3.99, 000000000);
 		Product product3 = new Product("Nvidia 1080 TI Graphics Card", 3, 899.99, 000000000);
+		
 		
 		inv.addProduct(product1);
 		inv.addProduct(product2);
 		inv.addProduct(product3);
 		
 		
+		int item3 = inv.SearchForQty("Nvidia 1080 TI Graphics Card");
+		int item2 = inv.SearchForQty("Pickles");
+		int item1 = inv.SearchForQty("Paper");
+		boolean qty1 = bo.isBo(item1);
+		boolean qty2 = bo.isBo(item2);
+		boolean qty3 = bo.isBo(item3);
+		int boQty1 = re.getOrdQty(qty1);
+		int boQty2 = re.getOrdQty(qty2);
+		int boQty3 = re.getOrdQty(qty3);
+		product1.setBackOrder(boQty1);
+		product2.setBackOrder(boQty2);
+		product3.setBackOrder(boQty3);
+		double price1 = inv.SearchForPrice("Paper");
+		double price2 = inv.SearchForPrice("Pickles");
+		double price3 = inv.SearchForPrice("Nvidia 1080 TI Graphics Card");
+		double resupply1 = re.SetOrdTotal(price1);
+		double resupply2 = re.SetOrdTotal(price2);
+		double resupply3 = re.SetOrdTotal(price3);
+		product1.setBackOrderTotal(resupply1);
+		product2.setBackOrderTotal(resupply2);
+		product3.setBackOrderTotal(resupply3);
+		String item = inv.searchByProductName("Nvidia 1080 TI Graphics Card");
+	
 		System.out.print(inv.toString());
-		String item = inv.searchByProductName("Pickles");
 		System.out.println(item);
-		//boolean found = inv.searchByProductName("Pickles");
-		//String item = inv.foundProduct(found, "Pickles");
-		//System.out.println(item);
-		//System.out.println(found);
+		System.out.println(qty2);
+		//System.out.println(re.toString());
 		
-		//System.out.println(inv.products[2]);
 
 	}
 
 }
-
